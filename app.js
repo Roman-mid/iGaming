f7bf22cf0b2100b7f25ce4e3561f0a02a4ead26f
Roman-mid <roman.midianko@gmail.com>
2025-05-01 16:20:44 +0100

    conect cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -11,11 +12,7 @@ const testAPIRouter = require('./routes/testAPI');
 
 const app = express();
 
-app.use(
-  cors({
-    origin: 'http://127.0.0.1:5500',
-  })
-);
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
