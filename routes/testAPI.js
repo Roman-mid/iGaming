0908a940e2c94551a4d90f2d11963a9a97071f44
Roman-mid <roman.midianko@gmail.com>
2025-05-01 16:26:34 +0100

    update json data

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.json({ title: 'API works! You are the best!!! VERY COOL!!!' });
+  res.json({
+    title: 'API works! You are the best!!! VERY COOL!!!',
+    description:
+      'Сделал первое API на express.js. Просто потестить как это работает.',
+    mode: 'test',
+    framework: 'express.js',
+  });
 });
 
 module.exports = router;
