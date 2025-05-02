dab214a16d81adde4bc58aaa32e59ca4c9b511f1
Roman-mid <roman.midianko@gmail.com>
2025-05-02 13:18:48 +0100

    change config.js

--- db/config.js
+++ db/config.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
   ssl: { rejectUnauthorized: false },
   family: 4,
 });
